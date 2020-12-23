2329d91b1c9c7b6c5307bcf2f458f60dc9424f78
Michael Lyon <mlyon360@gmail.com>
2020-12-23 11:56:24 -0700

    remove eslint add stylelint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { AppProps } from 'next/app'
 import 'styles/main.css'
 
-const App = ({ Component, pageProps }: any) => {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 
