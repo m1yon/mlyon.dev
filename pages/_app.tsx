ba1521bcfd5b447308f2bc89c56cb36ebf6173d9
Michael Lyon <mlyon360@gmail.com>
2020-12-26 19:06:58 -0700

    add dynamic header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,21 +4,29 @@ import { withDarkMode } from 'hooks/useDarkMode'
 import { useDarkMode } from 'hooks'
 import Head from 'next/head'
 import { Header, Hero } from 'components'
+import clsx from 'clsx'
 import 'tailwindcss/tailwind.css'
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps, router }: AppProps) => {
   const [darkMode] = useDarkMode()
 
+  const isHomepage = router.route === '/'
+
   return (
-    <div className={darkMode ? 'dark' : ''}>
+    <div className={clsx(darkMode && 'dark')}>
       <Head>
         <title>Michael Lyon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="text-gray-900 dark:text-gray-50 flex flex-col min-h-screen flex-1 items-center overflow-x-hidden bg-white dark:bg-gray-900">
-        <div className="bg-gray-50 dark:bg-gray-800 w-full overflow-hidden">
+        <div
+          className={clsx([
+            isHomepage && 'bg-gray-50 dark:bg-gray-800',
+            'w-full overflow-hidden',
+          ])}
+        >
           <Header />
-          <Hero />
+          {isHomepage ? <Hero /> : null}
         </div>
 
         <div className="container mx-auto pb-8 pt-16 xl:px-80 2xl:px-96">
