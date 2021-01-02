6a9fa4fa8486e191887f80a460398d7f030303f1
Michael Lyon <mlyon360@gmail.com>
2021-01-01 18:27:01 -0700

    add new 01-04-2020 post

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -3,13 +3,28 @@ import Head from 'next/head'
 
 type SEOProps = {
   title: string
+  url: string
+  description: string
+  image: string
 }
 
-const SEO = ({ title }: SEOProps) => {
+const SEO = ({ title, url, description, image }: SEOProps) => {
   return (
     <Head>
       <title>{title}</title>
       <link rel="icon" href="/favicon.ico" />
+
+      {/* Open Graph */}
+      <meta property="og:url" content={`https://mlyon.dev${url}`} />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={`https://mlyon.dev${image}`} />
+
+      {/* Twitter */}
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={`https://mlyon.dev${image}`} />
     </Head>
   )
 }
