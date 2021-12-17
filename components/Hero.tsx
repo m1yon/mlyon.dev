4139999b1e3eeddf5f0480621dfc6a0558811579
m1yon <mlyon360@gmail.com>
2021-12-17 14:47:30 -0700

    adjust hero image to use new NextJS image features

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
+import HeroImage from '../assets/img/hero.png'
 
 const Hero = () => {
   return (
     <div className="container mx-auto py-16 lg:py-40 pt-4 lg:pt-40 flex flex-col justify-center relative items-center lg:flex-row">
       <div className="block bg-white lg:bg-opacity-0 dark:bg-gray-900 lg:dark:bg-opacity-0 top-0 right-1/2 rounded-full lg:rounded-none lg:absolute w-52 h-52 lg:w-auto lg:h-auto overflow-hidden">
         <Image
-          src="/img/hero.png"
+          src={HeroImage}
           width={1143 / 4}
           height={2041 / 4}
           alt="Michael Lyon"
+          priority
+          placeholder="blur"
         />
       </div>
       <div className="text-center mt-4 lg:ml-80 lg:text-left lg:mt-0 mb-16 lg:mb-12">
